98e951b51cecdaf46dc349dab343bf54b10f1a64
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-23 01:28:08 +0600

    Install third party library, font awesome, react-fontawesome

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import './Product.css'
+import './Product.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+
+// npm command for Font Awesome
+// npm i--save @fortawesome/fontawesome-svg-core \
+// @fortawesome/free-solid-svg-icons \
+// @fortawesome/react-fontawesome
 
 const Product = (props) => {
     const { name, img, seller, price, stock } = props.product;
     return (
         <div className="product">
-            <div>
+            <div style={{marginRight:'5px'}}>
                 <img src={img} alt="" />
             </div>
             <div>
@@ -14,6 +21,7 @@ const Product = (props) => {
                 <p><small>by: {seller}</small></p>
                 <p>${price}</p>
                 <p><small>only {stock} left in stock - order soon</small></p>
+                <button className="cart-btn">  <FontAwesomeIcon icon={faShoppingCart} /> add to cart</button>
             </div>
         </div>
     );
