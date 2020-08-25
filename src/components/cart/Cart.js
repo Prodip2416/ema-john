044c791052e14247b5a67bcf1ebddb4889c4d55a
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-25 23:54:34 +0600

    ema john done

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -5,18 +5,22 @@ const Cart = (props) => {
     const cart = props.cart;
     const totalPrice = cart.reduce((total, item) => total + item.price, 0);
     let shipping = 0;
-    if (shipping > 35) {
+    if (totalPrice > 35) {
         shipping = 0
-    } else if (shipping > 15) {
+    } else if (totalPrice > 15) {
         shipping = 4.99;
     }
-    const grandTotal = (shipping + totalPrice).toFixed(2);
+    const grandTotal = shipping + totalPrice;
+
+    const convertToFixed = (num) => {
+        return num.toFixed(2);
+    }
     return (
         <div>
             <h3>Order Summery : {cart.length}</h3>
-            <p>Shipping : {shipping}</p>
-            <h4>Total Item Price : {totalPrice}</h4>
-            <h3 className="order-Total">Order Total : {grandTotal}</h3>
+            <p>Shipping : {convertToFixed(shipping)}</p>
+            <h4>Total Item Price : {convertToFixed(totalPrice)}</h4>
+            <h3 className="order-Total">Order Total : {convertToFixed(grandTotal)}</h3>
         </div>
     );
 };
