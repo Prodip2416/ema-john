ebe2ad7e6268ac3365582035c70e23979ec6bda9
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-30 00:32:37 +0600

    Save state on Localstore, store key and count, virtual DOM

--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -3,27 +3,31 @@ import fakeData from '../../fakeData/index';
 import './Shop.css';
 import Product from '../product/Product';
 import Cart from '../cart/Cart';
+import { addToDatabaseCart } from '../../utilities/databaseManager';
 
 const Shop = () => {
-    const first10 = fakeData.slice(0,10);
+    const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
     const [cart, setCart] = useState([]);
-  
-    const handleCartProduct = (product) =>{
+
+    const handleCartProduct = (product) => {
         const totalCartItem = [...cart, product];
         setCart(totalCartItem);
+
+        const totalLocalStorageProduct = totalCartItem.filter(item => item.key === product.key);
+        addToDatabaseCart(product.key, totalLocalStorageProduct.length);
     }
 
     return (
         <div className="shop-container">
             <div className="product-container">
                 {
-                    products.map(item => <Product product={item} handleCartProduct={handleCartProduct} showAddToCart={true}/>)
+                    products.map(item => <Product product={item} handleCartProduct={handleCartProduct} showAddToCart={true} />)
                 }
             </div>
-           <div className="cart-container">
-               <Cart cart={cart}/>
-           </div>
+            <div className="cart-container">
+                <Cart cart={cart} />
+            </div>
         </div>
     );
 };
