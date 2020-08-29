6d0bbbb60166c88ca8ea804b6cf261f0bf4cdba3
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-29 23:15:06 +0600

    Dynamic path, set route path parameter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,47 @@ import React from 'react';
 import './App.css';
 import Header from './components/header/Header';
 import Shop from './components/shop/Shop';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+import Revies from './components/Review/Review';
+import Review from './components/Review/Review';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/NotFound.js/NotFound';
+import ProductDetail from './components/ProductDetail/ProductDetail';
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Shop/>
+      <Header />
+
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Shop />
+          </Route>
+          <Route path="/shop">
+            <Shop />
+          </Route>
+          <Route path="/order">
+            <Review />
+          </Route>
+          <Route path="/inventory">
+            <Inventory />
+          </Route>
+          <Route path="/product/:key">
+            <ProductDetail />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Product.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 // npm command for Font Awesome
 // npm i--save @fortawesome/fontawesome-svg-core \
@@ -9,14 +10,14 @@ import {faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 // @fortawesome/react-fontawesome
 
 const Product = (props) => {
-    const { name, img, seller, price, stock } = props.product;
+    const { name, img, seller, price, stock , key} = props.product;
     return (
         <div className="product">
             <div style={{marginRight:'5px'}}>
                 <img src={img} alt="" />
             </div>
             <div>
-                <h4 className="product-name">{name}</h4>
+                <h4 className="product-name"> <Link to={"/product/"+key}>{name}</Link> </h4>
                 <p><small>by: {seller}</small></p>
                 <p>${price}</p>
                 <p><small>only {stock} left in stock - order soon</small></p>
